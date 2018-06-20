ac9f191882672bc3244fbec75fc35bbaaf3275e2
redletter25 <maia.dothager@yahoo.com>
2018-06-20 19:24:48 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,21 +1,21 @@
 
-def create_olympics_hash
+#def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
-end
+#end
 
-def add_a_key_value_pair
+#def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
+#end
 
-def iterate_through_hash
+#def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
+#end
 
-def iterate_through_keys
+#def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-end
+#end
 
 
 
